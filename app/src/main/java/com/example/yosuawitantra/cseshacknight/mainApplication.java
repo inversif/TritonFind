74fa1645c5efeb7b973085309bcaeae2f9da76e4
Jose Guan Zhou <generalcodygrievous@gmail.com>
2016-11-20 04:22:39 -0800

    Issues encountered with Firebase.

--- app/src/main/java/com/example/yosuawitantra/cseshacknight/mainApplication.java
+++ app/src/main/java/com/example/yosuawitantra/cseshacknight/mainApplication.java
@@ -1,6 +1,8 @@
 package com.example.yosuawitantra.cseshacknight;
 
 import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -8,22 +10,24 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class mainApplication extends Application {
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference database;
     private DataSnapshot currentSnapshot;
-    DatabaseReference reference = database.getReference("https://popping-torch-2292.firebaseio.com/");
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        FirebaseApp.initializeApp(this);
+        database = FirebaseDatabase.getInstance().getReference("https://popping-torch-2292.firebaseio.com/");
     }
 
     public void sendData(String type, Object item) {
-        DatabaseReference itemReference = reference.child(type);
+        DatabaseReference itemReference = database.child(type);
         itemReference.setValue(item);
     }
 
     public void getData() {
-        reference.addValueEventListener( new ValueEventListener() {
+        database.addValueEventListener( new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     currentSnapshot = dataSnapshot;
