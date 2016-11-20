60edc68f55d793823ac7c56ab6611f8fdfa4b56a
dianlin <dil048@ucsd.edu>
2016-11-20 04:44:38 -0800

    fixed main

--- app/src/main/java/com/example/yosuawitantra/cseshacknight/MainActivity.java
+++ app/src/main/java/com/example/yosuawitantra/cseshacknight/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.yosuawitantra.cseshacknight;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton Find;
+    ImageButton Home;
+    ImageButton Lost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Find = (ImageButton) findViewById(R.id.find);
+        Home = (ImageButton) findViewById(R.id.home);
+        Lost = (ImageButton) findViewById(R.id.lost);
+
+        Lost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainActivityIntent = new Intent(getApplication(), LostPost.class);
+                mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(mainActivityIntent);
+            }
+        });
+        Find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainActivityIntent = new Intent(getApplication(), FoundPost.class);
+                mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(mainActivityIntent);
+            }
+        });
+        Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainActivityIntent = new Intent(getApplication(), MainActivity.class);
+                mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(mainActivityIntent);
+            }
+        });
     }
 
 }
